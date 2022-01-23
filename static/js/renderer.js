5b5b87882c7e5247e1ccaa8ccdc34fcf48099d86
cosmoglint <ramprakashsmail@gmail.com>
2022-01-24 01:17:27 +0530

    ADD:  separate functions for linewise parsing

--- static/js/renderer.js
+++ static/js/renderer.js
@@ -38,6 +38,56 @@ function parseDiff(previous_lists, instance){
   return new_instance
 }
 
+function parseLine(previous_lists, instance){
+  original_list = previous_lists.original_list
+  updated_list = previous_lists.updated_list
+  old_count = previous_lists.old_count
+  new_count = previous_lists.new_count
+  value = instance.value;
+  count = instance.count;
+  new_instance = {original_list: original_list, updated_list: updated_list, old_count:old_count, new_count:new_count }
+  if (instance.added == undefined){
+    new_instance.original_list.push(value)
+    new_instance.new_count += count
+    new_editor.session.addMarker(new Range(1,1,1,count), "red", "text")
+  }
+  if ( instance.removed == undefined){
+    new_instance.updated_list.push(value)
+    new_instance.old_count += count
+    old_editor.session.addMarker(new Range(1,1,1,count), "green", "text")
+  }
+  return new_instance
+
+}
+
+function parseLists(old_list, new_list){
+  for (let i=0; i<old_list.length; i++){
+    old_val = old_list[i]
+    new_val = new_list[i]
+    diff_list = window.diff.diffChars(old_val, new_val)
+    previous_lists = {old_count:0, new_count:0 }
+    diff_list.forEach((instance) => {
+      old_count = previous_lists.old_count
+      new_count = previous_lists.new_count
+      value = instance.value;
+      count = instance.count;
+      new_instance = { old_count:old_count, new_count:new_count }
+      if (instance.added == undefined && instance.removed == undefined){
+        new_instance.old_count += count
+        new_instance.new_count += count
+      }
+      else if (instance.added == undefined){
+        old_editor.session.addMarker(new Range(i,new_count,i,old_count+count), "red", "text")
+        new_instance.new_count += count
+      }
+      else if ( instance.removed == undefined){
+        new_editor.session.addMarker(new Range(i,old_count,i,old_count+count), "green", "text")
+        new_instance.new_count += count
+      }
+    })
+  }
+}
+
 function findDiff(){
   // to do handle every change event and update the diff as necessary
   old_text = old_editor.getSession().getValue();
@@ -46,9 +96,9 @@ function findDiff(){
   let new_ll = new_editor.getSession().doc.getAllLines();
 
   //diff_list = window.diff.diffChars(old_text, new_text)
-  diff_list = window.diff.diffSentences(old_text, new_text)
-  console.log(diff_list)
-  let { original_list, updated_list } = diff_list.reduce(parseDiff, {original_list: [], updated_list: [], original_count:0, updated_count:0, column_count: 0, row_count: 0});
+  //diff_list = window.diff.diffSentences(old_text, new_text)
+  parseLists(old_ll, new_ll)
+  // let { original_list, updated_list } = diff_list.reduce(parseDiff, {original_list: [], updated_list: [], original_count:0, updated_count:0, column_count: 0, row_count: 0});
 
 }
 
